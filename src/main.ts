8d1766273f0ea15ea57f9ac0cb3383a5ca85e2ad
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-07-03 00:41:56 +0200

    add keywords, author & description

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 
 const DELAY = 50;
 
-// export const waitProp = 123;
-
 export const waitProp = (object: any, property: string, delay = DELAY) => {
   return new Promise(resolve => {
     const loop = () => {
